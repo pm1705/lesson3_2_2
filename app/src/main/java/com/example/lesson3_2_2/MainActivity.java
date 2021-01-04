cacf543993f637d80b6bd621852067d97d903cdf
Administrator <pm1705@bs.amalnet.k12.il>
2021-01-04 17:27:26 +0200

    final for 3_2

--- app/src/main/java/com/example/lesson3_2_2/MainActivity.java
+++ app/src/main/java/com/example/lesson3_2_2/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.lesson3_2_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button)findViewById(R.id.button);
+        i=0;
+    }
+
+    public void clicked(View view) {
+        btn.setText("clicke number "+i);
+        i++;
     }
 }
